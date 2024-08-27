94b57dcff7ea290360fad51c342c8071e189e778
BuildTools <unconfigured@null.spigotmc.org>
2024-08-27 16:19:33 +0800

    submit

--- src/constants.rs
+++ src/constants.rs
@@ -3,14 +3,13 @@ use std::cell::LazyCell;
 use regex::Regex;
 
 #[allow(unused)]
-pub(crate) const WHITE_SPACE: LazyCell<Vec<&'static str>> = LazyCell::new(|| {
-    vec!["\n", "\r", "\t", " "]
-});
+pub(crate) const WHITE_SPACE: LazyCell<Vec<&'static str>> =
+    LazyCell::new(|| vec!["\n", "\r", "\t", " "]);
 
-pub(crate) const WHITE_SPACE_REGEX: LazyCell<Regex> = LazyCell::new(|| {
-    Regex::new(r"\A\s").unwrap()
-});
+pub(crate) const WHITE_SPACE_REGEX: LazyCell<Regex> =
+    LazyCell::new(|| Regex::new(r"\A\s").unwrap());
 
+#[allow(dead_code)]
 #[cfg(debug_assertions)]
 pub(crate) const DEBUG: bool = true;
 
--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,12 @@ mod platform_const;
 
 use std::any::Any;
 
+use anyhow::{Ok, Result};
 use column_line::*;
-use constants::{DEBUG, WHITE_SPACE_REGEX};
-use derive_more::derive::{Display, Error};
+use constants::*;
+use derive_more::derive::Display;
 pub use matcher::*;
-
-type Res<T> = Result<T, TokenizerError>;
+use regex::Regex;
 
 #[derive(Clone)]
 pub struct Token {
@@ -56,8 +56,55 @@ impl Tokenizer {
         self.matchers.push(src.to_string().into());
     }
 
+    pub fn add_str_pattern_array<T: ToString, const N: usize>(&mut self, src: [T; N]) {
+        for s in src {
+            self.add_str_pat(s);
+        }
+    }
+
+    pub fn add_str_pattern_vec<T: ToString>(&mut self, src: Vec<T>) {
+        for s in src {
+            self.add_str_pat(s);
+        }
+    }
+
+    pub fn add_pattern_array<T: MatcherTrait + Any, const N: usize>(&mut self, src: [T; N]) {
+        for s in src {
+            self.add_pat(s);
+        }
+    }
+
+    pub fn add_pattern_vec<T: MatcherTrait + Any>(&mut self, src: Vec<T>) {
+        for s in src {
+            self.add_pat(s);
+        }
+    }
+
     pub fn add_pat<T: MatcherTrait + Any>(&mut self, src: T) {
         self.matchers.push(src.into());
+    }
+
+    pub fn add_regex_pat(&mut self, src: impl ToString) -> Result<()> {
+        self.matchers
+            .push(Regex::new(src.to_string().as_str())?.into());
+        Ok(())
+    }
+
+    pub fn add_regex_pattern_array<T: ToString, const N: usize>(
+        &mut self,
+        src: [T; N],
+    ) -> Result<()> {
+        for s in src {
+            self.add_regex_pat(s)?;
+        }
+        Ok(())
+    }
+
+    pub fn add_regex_pattern_vec<T: ToString>(&mut self, src: Vec<T>) -> Result<()> {
+        for s in src {
+            self.add_regex_pat(s)?;
+        }
+        Ok(())
     }
 
     pub fn add_ws_pat(&mut self) {
@@ -68,21 +115,17 @@ impl Tokenizer {
         self.matchers.push(src);
     }
 
-    pub fn start(&mut self) -> Res<Vec<Token>> {
-        if DEBUG {
-            dbg!(&self.str_iter);
-        }
+    pub fn start(&mut self) -> Result<Vec<Token>> {
         let mut res = Vec::with_capacity(self.matchers.len());
         let lookup = LineColLookup::new(&self.str_iter);
         let mut current_str = self.str_iter.clone();
         let mut current_index = 0;
         loop {
-            dbg!(&current_str);
             let mut matched = false;
             for reg in &self.matchers {
                 if let Some(s) = reg.get(&current_str) {
                     current_str = current_str[s.len()..].to_owned();
-                    let (line, column) = lookup.get_by_cluster(current_index);
+                    let (line, column) = lookup.get(current_index);
                     current_index += s.len();
                     res.push(Token {
                         val: s,
@@ -96,7 +139,7 @@ impl Tokenizer {
                 }
             }
             if !matched {
-                return Err(TokenizerError::AllRegexesMatchNothing);
+                return Err(TokenizerError::AllMatchersMatchNothing.into());
             }
             if current_str.len() == 0 {
                 break;
@@ -106,37 +149,34 @@ impl Tokenizer {
     }
 }
 
-#[derive(Debug, Display, Error)]
+#[derive(Debug, Display, thiserror::Error)]
 pub enum TokenizerError {
-    AllRegexesMatchNothing,
+    AllMatchersMatchNothing,
 }
 
 #[inline]
 pub fn build_tokenizer<TSrc: ToString>(val: Vec<Matcher>, src: TSrc) -> Tokenizer {
     let mut tokenizer = Tokenizer::new(src);
     for v in val {
-        tokenizer.add_common_pat(dbg!(v));
+        tokenizer.add_common_pat(v);
     }
     tokenizer
 }
 
 #[inline]
-pub fn to_tokens<TSrc: ToString>(val: Vec<Matcher>, src: TSrc) -> Res<Vec<Token>> {
+pub fn to_tokens<TSrc: ToString>(val: Vec<Matcher>, src: TSrc) -> Result<Vec<Token>> {
     let mut tokenizer = build_tokenizer(val, src);
     tokenizer.start()
 }
 
 #[inline]
-pub fn to_tokens_without_ws<TSrc: ToString>(val: Vec<Matcher>, src: TSrc) -> Res<Vec<Token>> {
+pub fn to_tokens_without_ws<TSrc: ToString>(val: Vec<Matcher>, src: TSrc) -> Result<Vec<Token>> {
     let mut tokenizer = build_tokenizer(val, src);
     Ok(filter_white_spaces(tokenizer.start()?))
 }
 
 #[inline]
 pub fn filter_white_spaces(val: Vec<Token>) -> Vec<Token> {
-    if DEBUG {
-        dbg!(&val);
-    }
     val.iter()
         .filter(|x| !WHITE_SPACE_REGEX.is_match(&x.val))
         .map(|x| x.clone())
@@ -169,5 +209,14 @@ class Test {
             .unwrap(),
         );
         dbg!(tokens);
+    }
+    #[test]
+    #[allow(unused_must_use)]
+    fn test_string_parse() {
+        let src = r#""sudsier\" asdf \"""#;
+        let mut tokenizer = Tokenizer::new(src);
+        tokenizer.add_ws_pat();
+        tokenizer.add_regex_pat(r#"\A"[[.[^"]]\\"]*""#).unwrap();
+        dbg!(tokenizer.start());
     }
 }
